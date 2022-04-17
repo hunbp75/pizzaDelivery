9e8445df65793232d2ce7493dea71cf6e7199d1f
hunbp75 <hunbp75@gmail.com>
2022-04-17 19:47:22 +0200

    add some text to the main page

---
+++ pizza/src/components/Text.jsx
@@ -0,0 +1,30 @@
+const Text = () => {
+  return (
+    <div className="textContainer">
+      <h1></h1>
+      <h4>
+        Benzinkútból kocsma, kocsmából hangulatos pizzéria. Kis családi
+        vállalkozásunk 1996-ban vette át az épületet amely klasszikus helyi
+        kocsmaként üzemelt. Előtte a korabeli fotók tanúsága szerint egy Shell
+        benzinkút üzemelt. 1998-ban indult el családias hangulatú pizzériánk
+        kicsi belső térrel pár asztallal majd később 2016-ban bővült a
+        téliesített előkerttel és nyerte el jelenlegi formáját, amely a hét
+        minden napján hétfő és kedd kivételével nyitva tart. Pizza, ahogy az
+        emberek szeretik... Aki klasszikus vékonytésztájú olasz pizzákra számít
+        az csalódni fog kínálatunkban, hiszen mi szándékosan kevertük a helyi,
+        magyaros ízeket az olasz konyha sajátosságaival. Nem másoltunk egyetlen
+        elterjedt ipari pizzaműhely termékeit, hanem még a tészta receptjét is
+        mi kísérleteztük ki, a feltétekben pedig igyekeztünk megjeleníteni a
+        magyar konyha bevett alapanyagait, szószait és fűszereit. Nem állítjuk,
+        hogy a helyi piacról szerezzük be az alapanyagokat, de a minőséget szem
+        előtt tartva csak ellenőrzött forrásból származó élelmiszereket
+        használunk fel a konyhában, mindig friss tésztát készítünk: akár naponta
+        többször is begyúrjuk, elkészítjük a pizzatésztát. A pizza pedig
+        hagyományos kézi módszerekkel frissen készül minden Kedves Vendégünk
+        számára.
+      </h4>
+    </div>
+  );
+};
+
+export default Text;
