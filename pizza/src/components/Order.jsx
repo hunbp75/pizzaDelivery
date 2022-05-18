0bb5b92d88d6627a858dacd75568240f5ae0f726
hunbp75 <hunbp75@gmail.com>
2022-05-18 13:45:15 +0200

    add some design

--- pizza/src/components/Order.jsx
+++ pizza/src/components/Order.jsx
@@ -15,14 +15,20 @@ const Order = (props) => {
   };
   return (
     <div>
-      <h1 className="skandinavian">Skandinavian Pizza</h1>
+      <div className="header-text">
+        <button type="button" className="bookatablebtn" onClick={openModal}>
+          - Asztalfoglalás -
+        </button>
+        <h1 className="skandinavian">Skandinavian Pizza</h1>
+        <div className="address">
+          <span>1010 Budapest, Pizza-szelet tér 8, (+36-55-555-5555)</span>
+        </div>
+      </div>
       <div className="container">
         <Menu />
         <Text />
       </div>
-      <button type="button" className="bookatablebtn" onClick={openModal}>
-        Asztalfoglalás
-      </button>
+
       <Modal isOpen={isOpen} closeModal={closeModal} />
     </div>
   );
