cfdfe935c523481a20c587b30622c3af15c22431
hunbp75 <hunbp75@gmail.com>
2022-05-18 13:47:24 +0200

    add useStates, useEffect and some functions

--- pizza/src/components/Modal.jsx
+++ pizza/src/components/Modal.jsx
@@ -1,15 +1,51 @@
-import { useState } from "react";
-import TableDate from "./TableDate";
-import TableTime from "./TableTime";
+import { useState, useEffect } from "react";
 
 const Modal = (props) => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [valid, setValid] = useState(false);
+  const [actualDate, setActualDate] = useState(null);
+  const [actualTime, setActualTime] = useState(null);
+  const [guest, setGuest] = useState(null);
+  const [today, setToday] = useState(null);
+  const [todayTime, setTodayTime] = useState(null);
 
   console.log("The name is: ", name);
   console.log("The emeail address: ", email);
   console.log("The email address is: valid? ", valid);
+
+  useEffect(() => {
+    let currentDate = new Date();
+    let ddd =
+      currentDate.getFullYear() +
+      "-" +
+      (currentDate.getMonth() + 1) +
+      "-" +
+      currentDate.getDate();
+    setToday(ddd);
+  }, []);
+
+  useEffect(() => {
+    let currentTime = new Date();
+    let time = currentTime.getHours() + ":" + currentTime.getMinutes();
+    setTodayTime(time);
+  }, []);
+
+  const sendData = () => {
+    return fetch("/api/booking", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        today,
+        todayTime,
+        name,
+        email,
+        guest,
+        actualDate,
+        actualTime,
+      }),
+    });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -36,6 +72,18 @@ const Modal = (props) => {
       setValid(true);
     }
     setEmail(value);
+  };
+
+  const handleChangeDate = (e) => {
+    setActualDate(e.target.value);
+  };
+
+  const handleChangeTime = (e) => {
+    setActualTime(e.target.value);
+  };
+
+  const handleChangeGuest = (e) => {
+    setGuest(e.target.value);
   };
 
   return (
@@ -69,6 +117,7 @@ const Modal = (props) => {
                 placeholder="Vendégek száma:"
                 min="1"
                 max="30"
+                onChange={handleChangeGuest}
               />
               <input
                 type="email"
@@ -78,10 +127,21 @@ const Modal = (props) => {
                 onChange={handleChangeEmail}
               />
               <div className="dateTime">
-                <TableDate />
-                <TableTime />
+                <div className="date">
+                  <h4>A foglalás napja</h4>
+                  <input type="date" id="datebtn" onChange={handleChangeDate} />
+                </div>
+                <div className="date">
+                  <h4>A foglalás időpontja</h4>
+                  <input
+                    type="time"
+                    id="datebtn"
+                    onChange={handleChangeTime}
+                  ></input>
+                </div>
+                );
               </div>
-              <button type="submit" id="submitbtn">
+              <button type="submit" id="submitbtn" onClick={() => sendData()}>
                 Foglalás
               </button>
             </form>
